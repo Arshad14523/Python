9e83b3632e7d5b43fa5e45f6f7511cf9e553e473
Arshad14523 <arshadkhanpathan891@gmail.com>
2026-02-03 19:23:15 +0530

    python

--- Day one.py
+++ Day one.py
@@ -1,2 +1,10 @@
 for i in range(1, 11):
     print(i)
+# go to the project folder (no filename)
+cd "C:\Users\Asus\OneDrive\Desktop\python coding practice"
+
+# show files to confirm you're in the right place
+ls -Force
+
+# check git status (repo already initialized)
+git status
